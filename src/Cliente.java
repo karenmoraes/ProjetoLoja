1eb191056fa5f414f4b3e87908e57642ab5a66f1
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 10:52:17 -0300

    Adicionar Classe Pessoa Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,49 @@
+
+public class Cliente extends Pessoa{
+    private String codigo;
+    private String email;
+    private int dependente;
+    private double salario;
+    private double limite;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDependente() {
+        return dependente;
+    }
+
+    public void setDependente(int dependente) {
+        this.dependente = dependente;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+}
