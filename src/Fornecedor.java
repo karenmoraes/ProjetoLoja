2c00f2ee31ffe678a15e8c36028d8f20ef72659a
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 10:56:36 -0300

    Adicionar Classe Categoria Produto Fornecedor

---
+++ src/Fornecedor.java
@@ -0,0 +1,29 @@
+
+public class Fornecedor extends CategoriaProduto{
+    
+    private String nome;
+    private String cnpj;
+    private String telefone;
+    
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+   
+
+    
+}
