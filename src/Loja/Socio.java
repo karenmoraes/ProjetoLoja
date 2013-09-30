4a01c362ce841e22ea979a73f874d4befcc3ad45
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 11:16:19 -0300

    Adicionar Classe Pessoa Socio

---
+++ src/Loja/Socio.java
@@ -0,0 +1,27 @@
+
+package Loja;
+
+
+public class Socio extends Pessoa{
+    private String cnpj;
+    private String enpresa;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEnpresa() {
+        return enpresa;
+    }
+
+    public void setEnpresa(String enpresa) {
+        this.enpresa = enpresa;
+    }
+    
+    
+    
+}
