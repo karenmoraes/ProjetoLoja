af527fdea668a66a379bc2a13c26c6b2ffc1b58e
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 11:11:38 -0300

    Adicionar Classe Categoria Produto Produto

---
+++ src/Loja/Produto.java
@@ -0,0 +1,35 @@
+package Loja;
+
+
+
+import java.util.Date;
+
+
+
+
+
+public class Produto extends CategoriaProduto{
+    
+    private String qntEstoque;
+    private Date datacompra;
+
+    public Date getDatacompra() {
+        return datacompra;
+    }
+
+    public void setDatacompra(Date datacompra) {
+        this.datacompra = datacompra;
+    }
+
+    public String getQntEstoque() {
+        return qntEstoque;
+    }
+
+    public void setQntEstoque(String qntEstoque) {
+        this.qntEstoque = qntEstoque;
+    }
+    public Produto(){
+        
+    }
+    
+}
