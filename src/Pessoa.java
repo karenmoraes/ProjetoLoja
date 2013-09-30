d3a7e4084343bd3135ec5594cfb0b3f8a2953fac
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 10:44:45 -0300

    Adicionar Classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,64 @@
+
+import java.util.Date;
+
+
+public class Pessoa {
+    
+    private String nome;
+    private String cpf;
+    private String rg;
+    private Date datanascimento;
+    private int sexo;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Date getDatanascimento() {
+        return datanascimento;
+    }
+
+    public void setDatanascimento(Date datanascimento) {
+        this.datanascimento = datanascimento;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+   
+    
+}
