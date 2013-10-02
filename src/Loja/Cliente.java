020a253df6fdc917f632d6c35db3c7d6fee8870c
ezio_joao <ezio_joao@SNCUA1ELI1ET029>
2013-10-02 01:17:28 -0300

    Adicionar Pessoa Cliente

--- src/Loja/Cliente.java
+++ src/Loja/Cliente.java
@@ -1,5 +1,7 @@
 package Loja;
 
+import java.util.Date;
+
 
 
 
@@ -8,12 +10,24 @@ public class Cliente extends Pessoa {
     private String codigo;
     private String email;
     private int dependente;
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "codigo=" + codigo + ", email=" + email + ", dependente=" + dependente + ", salario=" + salario + ", limite=" + limite + '}';
+    }
     private double salario;
     private double limite;
 
-    public Cliente(){
-        
+    public Cliente(String codigo, String email, int dependente, double salario, double limite, String nome, String cpf, String rg, Date datanascimento, int sexo, String telefone, String rua, String bairro, String cidade, int numero, String cep) {
+        super(nome, cpf, rg, datanascimento, sexo, telefone, rua, bairro, cidade, numero, cep);
+        this.codigo = codigo;
+        this.email = email;
+        this.dependente = dependente;
+        this.salario = salario;
+        this.limite = limite;
     }
+
+  
     public String getCodigo() {
         return codigo;
     }
