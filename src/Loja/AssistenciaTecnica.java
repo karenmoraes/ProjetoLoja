364f2865d200ddc4a158674af11812b47d6caef5
ezio_joao <ezio_joao@SNCUA1ELI1ET029>
2013-10-02 01:23:03 -0300

    Adicionar Assistencia Tecnica

--- src/Loja/AssistenciaTecnica.java
+++ src/Loja/AssistenciaTecnica.java
@@ -1,19 +1,44 @@
 
 package Loja;
 
-import javax.swing.JOptionPane;
+import java.util.Date;
+
+
 
 public class AssistenciaTecnica extends Cliente {
     private Produto p = new Produto();
     private String descricao;
+
+    public AssistenciaTecnica(String descricao, String codigo, String email, int dependente, double salario, double limite, String nome, String cpf, String rg, Date datanascimento, int sexo, String telefone) {
+        super(codigo, email, dependente, salario, limite, nome, cpf, rg, datanascimento, sexo, telefone);
+        this.descricao = descricao;
+    }
+
+  
+
+    public Produto getP() {
+        return p;
+    }
+
+    public void setP(Produto p) {
+        this.p = p;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "AssistenciaTecnica{" + "p=" + p + ", descricao=" + descricao + '}';
+    }
     
-    public void conserto(){
-        p.getCodigo();
-        p.getNome();
-        p.getDatacompra();
-       descricao = JOptionPane.showInputDialog("Informe o uqe precisa ser consertado ou o que não esta funcionando");
-       
+   
     }
     
     
-}
+
